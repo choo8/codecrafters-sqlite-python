014ebf20237a9644c21895cc0da4089602183e1d
choo8 <choo8@illinois.edu>
2022-11-09 01:48:06 -0800

    Completed Stage 1

--- app/main.py
+++ app/main.py
@@ -66,6 +66,6 @@ if command == ".dbinfo":
         print("Logs from your program will appear here!")
 
         # Uncomment this to pass the first stage
-        # print(f"number of tables: {len(sqlite_schema_rows)}")
+        print(f"number of tables: {len(sqlite_schema_rows)}")
 else:
     print(f"Invalid command: {command}")
